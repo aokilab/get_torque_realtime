492c44dc5b124126cef80311eafbc75b07f6ce44
tet-tnk <tetx2a@gmail>
2023-11-22 16:35:04 +0900

    Add torqueGet
    Can get torque real-time

--- GetTorque.py
+++ GetTorque.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python2
-# -*- coding: utf-8 -*-
+#! /usr/bin/env python2
 
 import numpy as np
 import rospy
@@ -7,4 +6,7 @@ import rospy
 from std_msgs.msg import Int32
 
 def callback(msg):
-    
+    print(msg.effort[0])
+
+
+sub = rospy.Subscriber('/torobo/joint_states', Int32, callback)
---
+++ torqueGet.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import rospy
+from sensor_msgs.msg import JointState
+import numpy as np
+
+# グローバル変数（トルクデータを格納する行列）
+torque_matrix = np.zeros((1, 7))
+
+def callback(data):
+    global torque_matrix
+    # トルクデータを行列に格納
+    torque_matrix = np.array([data.effort])
+
+def main():
+    rospy.init_node("joint_states_listener", anonymous=True)
+    rospy.Subscriber("/torobo/joint_states", JointState, callback)
+
+    # ここでトピックが更新されるたびにコールバック関数が呼ばれる
+    # トルクデータは1行7列の行列に格納される
+
+    while not rospy.is_shutdown():
+        # トルクデータを表示
+        print("Torque Matrix:")
+        print(torque_matrix)
+        rospy.sleep(0.01)  # ループの頻度を調整するために少し待つ
+
+if __name__ == "__main__":
+    main()
